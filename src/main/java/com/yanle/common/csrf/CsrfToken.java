6e6a47ee7ab762c8c6965e6f9aee65319d91dc7e
leyan <417467928@qq.com>
2017-04-24 17:10:14 +0800

    完善角色管理

---
+++ src/main/java/com/yanle/common/csrf/CsrfToken.java
@@ -0,0 +1,10 @@
+package com.yanle.common.csrf;
+
+/**
+ * Csrf过滤注解
+ *
+ * @author yanle
+ * @creator 2017-04-01 15:04
+ **/
+public @interface CsrfToken {
+}
--- src/main/java/com/yanle/controller/RoleController.java
+++ src/main/java/com/yanle/controller/RoleController.java
@@ -4,6 +4,7 @@ import com.yanle.entity.PageInfo;
 import com.yanle.service.IRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,4 +42,11 @@ public class RoleController extends BaseController {
         roleService.selectDataGrid(pageInfo);
         return pageInfo;
     }
+
+    @GetMapping("/addPage")
+    public String addPage(){
+        return "roleAdd";
+    }
+
+
 }
